81bc8d4d7fc72c1b04304e909c9c92ebf4c69dca
cyrille.davin <cyrille.davin@ASTPB0TCMH2.office.pbmsprod.pvt>
2018-02-19 15:53:47 +0100

    Init

---
+++ src/main/java/com/cysomel/sbfrontend/SbfrontendApplication.java
@@ -0,0 +1,12 @@
+package com.cysomel.sbfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbfrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbfrontendApplication.class, args);
+	}
+}
---
+++ src/main/java/com/cysomel/sbfrontend/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.cysomel.sbfrontend;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SbfrontendApplication.class);
+	}
+
+}
